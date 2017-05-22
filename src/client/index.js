0ae6946020a4dab83846ef8f89d13dec99eb15e8
perillaroc <perillaroc@gmail.com>
2017-05-22 21:36:04 +0800

    use menu button to change background.

--- src/client/index.js
+++ src/client/index.js
@@ -9,20 +9,21 @@ const welcome_background = electron.remote.getGlobal('welcome_background');
 
 window.addEventListener('load', function(event){
     document.body.style.backgroundImage = "url(" + welcome_background +")";
+
+    document.querySelector('#random_button').addEventListener('click', function(event){
+        const image_list = electron.remote.getGlobal('image_list');
+        let image_count = image_list.length;
+        if(image_count === 0 ){
+            return
+        }
+        index = Math.floor(Math.random() * image_count);
+        let image_url = image_list[index].src;
+        console.log(image_url);
+        let background_image = new Image();
+        background_image.onload = function(event){
+            document.body.style.backgroundImage = "url("+ this.src +")";
+        };
+        background_image.src = image_url;
+    });
 });
 
-document.body.addEventListener('click', function(event){
-    const image_list = electron.remote.getGlobal('image_list');
-    let image_count = image_list.length;
-    if(image_count === 0 ){
-        return
-    }
-    index = Math.floor(Math.random() * image_count);
-    let image_url = image_list[index].src;
-    console.log(image_url);
-    let background_image = new Image();
-    background_image.onload = function(event){
-        document.body.style.backgroundImage = "url("+ this.src +")";
-    };
-    background_image.src = image_url;
-});
